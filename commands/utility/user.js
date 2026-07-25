0c426b8c4c7605ccc8162ed2354805c49712e973
Vedant <vedantbhangale7@gmail.com>
2026-07-26 00:18:57 +0530

    Initializing the Discord bot with some commands.

---
+++ commands/utility/user.js
@@ -0,0 +1,8 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder().setName('user').setDescription('Provides information about the user.'),
+    async execute(message) {
+        await message.reply(`This command was run by ${message.user.username}, who joined on ${message.member.joinedAt} `);
+    }
+}
