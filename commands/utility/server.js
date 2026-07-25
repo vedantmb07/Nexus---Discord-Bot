9632a6734135edfca4a9bdeedce23b70a23537e3
Vedant <vedantbhangale7@gmail.com>
2026-07-26 00:40:16 +0530

    Added command handler and event listener.

---
+++ commands/utility/server.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder().setName('server').setDescription('Provides information about the server.'),
+	async execute(message) {
+		
+		await message.reply(
+			`This server is ${message.guild.name} and has ${message.guild.memberCount} members.`,
+		);
+	},
+};
--- index.js
+++ index.js
@@ -1,11 +1,61 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 const { token } = require('./config.json');
+const fs = require("node:fs");
+const path = require("node:path");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, (readyClient) => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
+
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for(const folder of commandFolders){
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+    
+    for(const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        
+        if('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
+        }
+    }
+}
+
+client.on(Events.InteractionCreate, async (message) => {
+	if (!message.isChatInputCommand()) return;
+	const command = message.client.commands.get(message.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${message.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(message);
+	} catch (error) {
+		console.error(error);
+		if (message.replied || message.deferred) {
+			await message.followUp({
+				content: 'There was an error while executing this command!',
+				flags: MessageFlags.Ephemeral,
+			});
+		} else {
+			await message.reply({
+				content: 'There was an error while executing this command!',
+				flags: MessageFlags.Ephemeral,
+			});
+		}
+	}
+});
+
 
 
 client.login(token);
