be98880120258023e2a5c8bae03e5e0bfd6aea8e
Vedant <vedantbhangale7@gmail.com>
2026-07-26 12:41:17 +0530

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,5 +1,5 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+const { clientID, guildID, token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 const commands = [];
@@ -27,7 +27,7 @@ const rest = new REST().setToken(token);
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
-		const data = await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
+		const data = await rest.put(Routes.applicationGuildCommands(clientID, guildID), { body: commands });
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
 		console.error(error);
