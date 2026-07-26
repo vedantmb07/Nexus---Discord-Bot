892569c0afe3bb0f77842048e39460a3163ec681
Vedant <vedantbhangale7@gmail.com>
2026-07-26 14:32:59 +0530

    Added Cooldowns to commands.

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
+    cooldown: 5,
     data: new SlashCommandBuilder().setName('user').setDescription('Provides information about the user.'),
     async execute(message) {
         await message.reply(`This command was run by ${message.user.username}, who joined on ${message.member.joinedAt} `);
